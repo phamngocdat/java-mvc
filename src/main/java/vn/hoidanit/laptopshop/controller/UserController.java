7bd0d5196c1a9446f21d98c71d029cbcc4969162
phamngocdat <ngocdat199614@gmail.com>
2024-11-27 22:54:29 +0700

    update code mvc

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,35 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+@Controller
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/hoidanit")
+    public  String getHomePage(){
+        String test = this.userService.handleHello();
+        return "eric.html";
+    }
+}
+
+// @RestController
+// public class UserController {
+//     private UserService userService;
+
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
+
+//     @GetMapping("/")
+//     public  String getHomePage(){
+//         return this.userService.handleHello();
+//     }
+// }
